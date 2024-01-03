a6c0dd34c511d00421f2de2a064f6d5b0bde6dec
Ritesh <ritesh.sonar111@gmail.com>
2024-01-03 22:12:35 +0530

    Coroutine added

---
+++ app/src/main/java/com/example/coroutinesexamples/Calculater.kt
@@ -0,0 +1,12 @@
+package com.example.coroutinesexamples
+
+
+class Calculator {
+    fun add(a: Int, b: Int): Int {
+        return a + b
+    }
+
+    fun subtract(a: Int, b: Int): Int {
+        return a - b
+    }
+}
---
+++ app/src/test/java/com/example/coroutinesexamples/CalculatorTest.kt
@@ -0,0 +1,21 @@
+package com.example.coroutinesexamples
+
+import org.junit.Assert
+import org.junit.Test
+
+
+class CalculatorTest {
+    @Test
+    fun testAdd() {
+        val calculator = Calculator()
+        val result = calculator.add(2, 3)
+        Assert.assertEquals(5, result.toLong())
+    }
+
+    @Test
+    fun testSubtract() {
+        val calculator = Calculator()
+        val result = calculator.subtract(5, 2)
+        Assert.assertEquals(3, result.toLong())
+    }
+}
