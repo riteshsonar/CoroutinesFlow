a471df445edaa3399bb921a6e9b5ff2b4de8096d
Ritesh <ritesh.sonar111@gmail.com>
2024-01-11 23:03:20 +0530

    constructor added

--- app/src/main/java/com/example/coroutinesexamples/KoltinTutorial/Constructors.kt
+++ app/src/main/java/com/example/coroutinesexamples/KoltinTutorial/Constructors.kt
@@ -5,10 +5,13 @@ fun main(){
     var car2 = Automobile("car2")
 }
 
+//Primary constructor:-
 class Automobile(val name:String, val tyres:Int, val  maxSetting:Int, hasAirBags: Boolean){ //2 property one parameter
     //if you are use val or val in function then it create it's property other wise it create only parameter
     val hasAir=hasAirBags
 
+    /*Secondary constructor:- secondary constructor is created by using the keyword constructor. It is used to initialize
+    a group of values. in kotlin a single class can have more then one secondary constructor*/
     constructor(nameParam:String):this(nameParam,4,5,true)//when we want to pass different value in first constructor
     fun drive(){}
     fun applyBrakes(){}
---
+++ app/src/main/java/com/example/coroutinesexamples/Retrofit/RetrofitActivity.kt
@@ -0,0 +1,12 @@
+package com.example.coroutinesexamples.Retrofit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.coroutinesexamples.R
+
+class RetrofitActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_retrofit)
+    }
+}
