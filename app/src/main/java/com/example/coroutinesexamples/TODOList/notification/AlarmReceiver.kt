ba70d3b75f3af42e57eee06c3a01b2109e8fbd7d
Ritesh <ritesh.sonar111@gmail.com>
2024-01-16 20:11:16 +0530

    Add Broadcast for notification

--- app/src/main/java/com/example/coroutinesexamples/TODOList/notification/AlarmReceiver.kt
+++ app/src/main/java/com/example/coroutinesexamples/TODOList/notification/AlarmReceiver.kt
@@ -7,14 +7,25 @@ import android.app.Service
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import com.example.coroutinesexamples.TODOList.Database.ToDoListDatabase
+import java.util.Date
 
 class AlarmReceiver : BroadcastReceiver() {
 private val GROUP_MESSAGE : String = "TODOList"
 
+    var toDoListDatabse: ToDoListDatabase? = null
     @SuppressLint("WrongConstant")
     override fun onReceive(context: Context?, intent: Intent?) {
-        //context?.let{ initiateDatabase(it)}
+        context?.let{ initiateDatabase(it)}
         val notificationManager: NotificationManager =context?.getSystemService(Service.NOTIFICATION_SERVICE) as NotificationManager
         val isShow =intent?.getIntExtra("isShow",0)?:0
+
     }
+
+    private fun initiateDatabase(context: Context?){
+        if (toDoListDatabse == null){
+            toDoListDatabse = ToDoListDatabase.getInstance(context!!)
+        }
+    }
+fun getNumber() : Int = (Date().time/1000L % Integer.MAX_VALUE).toInt()
 }
